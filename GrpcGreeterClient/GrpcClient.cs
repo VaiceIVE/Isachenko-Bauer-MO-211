96848479cf9b829c3cb28f8d5c45fa972b0f3e1a
Ivan Isachenko <109348025+VaiceIVE@users.noreply.github.com>
2024-12-11 16:16:51 +0600

    Update GrpcClient.cs

--- GrpcGreeterClient/GrpcClient.cs
+++ GrpcGreeterClient/GrpcClient.cs
@@ -6,11 +6,11 @@ namespace GrpcGreeterClient;
 
 public class GrpcClient
 {
-    public static async void Call(string ip, string message)
+    public static void Call(string ip, string message)
     {
         using var channel = GrpcChannel.ForAddress(ip);
         var client = new Greeter.GreeterClient(channel);
-        var response = client.AcceptGameAsync(new serializedGameMessage{SerializedGame = message});
+        var response = client.AcceptGame(new serializedGameMessage{SerializedGame = message});
         Console.WriteLine(response);
     }
 }
